3e325c7e06ac9bc8518b60760b246d2b5f74e5ba
giladrozmarin <19gilad@gmail.com>
2020-12-12 18:41:57 +0200

    Day12

---
+++ Day12/main.js
@@ -0,0 +1,57 @@
+function validTime(str) {
+    //  write code here.
+    //assume I get a valid string with natural numbers
+    const time = str.split(':');
+
+    return  parseInt(time[0]) < 24 && parseInt(time[1]) < 60 ? true : false
+
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
