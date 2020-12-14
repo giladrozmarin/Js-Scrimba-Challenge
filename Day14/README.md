0cf8c08c77619cf30f0242c48d20aea1642f71b9
giladrozmarin <19gilad@gmail.com>
2020-12-14 10:50:16 +0200

    Day14

---
+++ Day14/README.md
@@ -0,0 +1,11 @@
+
+  # Js-Scrimba-Challenge
+<p align="center">
+24 days of java script challenge from scrimba.com
+  </p>
+<h3 align="center">
+ Day14
+  </h3>
+<p align="center">
+<img src="./Day14.png" width="600" height="350">
+  </p>
---
+++ Day14/main.js
@@ -0,0 +1,41 @@
+function arrayMaximalAdjacentDifference(nums) {
+    //  write code here.
+    
+    return Math.max(...nums.map((val,i)=>Math.abs(val-nums[i+1])).filter(val=>val))
+    
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('arrayMaximalAdjacentDifference()', () => {
+    it('returns largest difference between adjacent values', () => {
+        // arrange
+        const nums = [2, 4, 1, 0];
+        
+        // act
+        const result = arrayMaximalAdjacentDifference(nums);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(3);
+    });
+
+    it('returns largest difference between adjacent values example 2', () => {
+        // arrange
+        const nums = [2, 9, 1, 0];
+        
+        // act
+        const result = arrayMaximalAdjacentDifference(nums);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(8);
+    });
+});
