997840812a545451ba80355d157cd88eb6f370d5
giladrozmarin <19gilad@gmail.com>
2020-12-16 10:38:00 +0200

    Day16

---
+++ Day16/README.md
@@ -0,0 +1,11 @@
+
+  # Js-Scrimba-Challenge
+<p align="center">
+24 days of java script challenge from scrimba.com
+  </p>
+<h3 align="center">
+ Day16
+  </h3>
+<p align="center">
+<img src="./Day16.png" width="600" height="350">
+  </p>
---
+++ Day16/main.js
@@ -0,0 +1,26 @@
+function insertDashes(arr) {
+    // write code here
+   
+    return arr.split('').join('-').replace(/- -/g,' ')
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('insertDashes()', () => {
+    it('insert dashes in between chars', () => {
+        // arrange
+        const value = "aba caba ac";
+        
+        // act
+        const result = insertDashes(value);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe("a-b-a c-a-b-a a-c");
+    });
+});
